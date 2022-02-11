215e0f1ed2bda5aed30a57e2b023c9f11f28022c
sagunaukade <sagunaukade96@gmail.com>
2022-02-11 13:17:23 +0530

    [Saguna] Added Welcome Message

---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to the employee wage computation programs");
+            Console.ReadLine();
+
+        }
+    }
+}
